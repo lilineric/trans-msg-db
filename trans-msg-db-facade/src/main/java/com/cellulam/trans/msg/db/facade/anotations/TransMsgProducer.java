b39c24f3c5bf64d449a2cf40f7ec8907edbf76b6
lilin <shoring@outlook.com>
2022-06-09 21:46:50 +0800

    init

---
+++ trans-msg-db-facade/src/main/java/com/cellulam/trans/msg/db/facade/anotations/TransMsgProducer.java
@@ -0,0 +1,17 @@
+package com.cellulam.trans.msg.db.facade.anotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * producer for trans message
+ *
+ * @author eric.li
+ * @date 2022-06-09 21:41
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TransMsgProducer {
+}
